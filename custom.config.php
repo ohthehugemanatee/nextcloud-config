1f7bb5bb9325bdb83edddbcf26aeedc09705a84a
Campbell Vertesi <campbell@vertesi.com>
2018-10-20 00:41:57 +0200

    Rename config file

---
+++ custom.config.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @file
+ * Custom Nextcloud config.
+ */
+
+// Ignore the filesystem permissions check. Blob storage comes up as world-writable.
+$CONFIG['check_data_directory_permissions'] = FALSE;
--- filesystem.config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/**
- * @file
- * Nextcloud config to get the Azure blob filesystem working.
- */
-
-$CONFIG['check_data_directory_permissions'] = FALSE;
